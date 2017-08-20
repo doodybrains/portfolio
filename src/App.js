bfa5a747d1a425623452dd3045bdd6e01d0f9b6f
Emma <doodybrainz@gmail.com>
2017-08-20 15:04:14 -0400

    add files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app-wrapper">
+        <h1>app</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
